36bbfcbd406247a7e516595c53654b0b864f0d5a
muluk.d <muluk.dharmayana@gmail.com>
2021-01-13 18:37:06 +0700

    perubahan pada HomeController

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,10 +1,18 @@
-<?php namespace App\Controllers;
+<?php 
+
+namespace App\Controllers;
 
 class Home extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message');
+		// return view('welcome_message');
+		echo "Hello World test ini function Home controller";
+	}
+
+	public function coba()
+	{
+		echo "ini adalah Method Coba";
 	}
 
 	//--------------------------------------------------------------------
